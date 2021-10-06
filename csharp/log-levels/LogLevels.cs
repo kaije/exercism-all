e202472adaedca4d09178f07df71c7fbd125e924
Kelly Farlow <kelly.farlow@gmail.com>
2021-10-06 14:11:31 +1000

    csharp/log-levels: Reformat

--- csharp/log-levels/LogLevels.cs
+++ csharp/log-levels/LogLevels.cs
@@ -8,5 +8,6 @@ static class LogLine
     public static string LogLevel(string logLine) => 
         logLine.Substring(logLine.IndexOf('[') + 1,logLine.IndexOf(']') - 1).ToLower();
 
-    public static string Reformat(string logLine) => $"{Message(logLine)} ({LogLevel(logLine)})";
+    public static string Reformat(string logLine) => 
+        $"{Message(logLine)} ({LogLevel(logLine)})";
 }
