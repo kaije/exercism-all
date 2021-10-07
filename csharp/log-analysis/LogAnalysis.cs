e90220a302110f7ba23ce3b66d06782eab3ac448
Kelly Farlow <kelly.farlow@gmail.com>
2021-10-07 15:50:06 +1000

    csharp/log-analysis: Implement SubstringBetween method

--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -6,7 +6,11 @@ public static class LogAnalysis
         return str.Split(delimeter)[1];
     }
 
-    public static string SubstringBetween(this string str, string firstDelimeter, string secondDelimeter) => "not implemented";
+    public static string SubstringBetween(this string str, string firstDelimeter, string secondDelimeter) {
+        int first = str.IndexOf('[') + 1;
+        int last = str.IndexOf(']');
+        return str.Substring(first, last - first);
+    }
     
     public static string Message(this string str) => "not implemented";
 
