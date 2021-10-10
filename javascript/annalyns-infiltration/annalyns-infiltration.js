4b6ee6da5ee3c8a4bd8ed1d020629b15bfbedea7
Kelly Farlow <kelly.farlow@gmail.com>
2021-10-10 15:00:59 +1000

    javascript/annalyns-infiltration: Remove comments and tidy up

--- javascript/annalyns-infiltration/annalyns-infiltration.js
+++ javascript/annalyns-infiltration/annalyns-infiltration.js
@@ -1,50 +1,11 @@
-/**
- * The fast attack is available when the knight is sleeping
- *
- * @param {boolean} knightIsAwake
- *
- * @return {boolean} Whether or not you can execute a fast attack.
- */
 export const canExecuteFastAttack = (knightIsAwake) => 
   !knightIsAwake;
 
-/**
- * A useful spy captures information, which they can't do if everyone's asleep.
- *
- * @param {boolean} knightIsAwake
- * @param {boolean} archerIsAwake
- * @param {boolean} prisonerIsAwake
- *
- * @returns {boolean} Whether or not you can spy on someone.
- */
 export const canSpy = (knightIsAwake, archerIsAwake, prisonerIsAwake) =>
   knightIsAwake || archerIsAwake || prisonerIsAwake;
 
-/**
- * You'll get caught by the archer if you signal while they're awake.
- *
- * @param {boolean} archerIsAwake
- * @param {boolean} prisonerIsAwake
- *
- * @returns {boolean} Whether or not you can send a signal to the prisoner.
- */
 export const canSignalPrisoner = (archerIsAwake, prisonerIsAwake) =>
   prisonerIsAwake && !archerIsAwake;
 
-/**
- * The final stage in the plan: freeing Annalyn's best friend.
- *
- * @param {boolean} knightIsAwake
- * @param {boolean} archerIsAwake
- * @param {boolean} prisonerIsAwake
- * @param {boolean} petDogIsPresent
- *
- * @returns {boolean} Whether or not you can free Annalyn's friend.
- */
-export const canFreePrisoner = (
-  knightIsAwake,
-  archerIsAwake,
-  prisonerIsAwake,
-  petDogIsPresent
-) => 
+export const canFreePrisoner = (knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent) => 
   !archerIsAwake && (petDogIsPresent || (prisonerIsAwake && !knightIsAwake));
