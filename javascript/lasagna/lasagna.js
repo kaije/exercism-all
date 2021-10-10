400ba100760f43688d188d3fba35c5e2f6f3a265
Kelly Farlow <kelly.farlow@gmail.com>
2021-10-10 14:42:01 +1000

    javascript/lasagne: Wrap all function bodies

--- javascript/lasagna/lasagna.js
+++ javascript/lasagna/lasagna.js
@@ -4,7 +4,8 @@ export const EXPECTED_MINUTES_IN_OVEN = 40;
 export const remainingMinutesInOven = (actualMinutesInOven) => 
   EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
 
-export const preparationTimeInMinutes = (numberOfLayers) => PREPARATION_MINUTES_PER_LAYER * numberOfLayers;
+export const preparationTimeInMinutes = (numberOfLayers) => 
+  PREPARATION_MINUTES_PER_LAYER * numberOfLayers;
 
 export const totalTimeInMinutes = (numberOfLayers, actualMinutesInOven) => 
   preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
