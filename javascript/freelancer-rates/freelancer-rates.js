4bfa7df28b0d6655833bef9f29335d5cff789c6d
Kelly Farlow <kelly.farlow@gmail.com>
2021-10-11 23:06:45 +1000

    javascript/freelancer-rate: Implement monthRate with applyDiscount

--- javascript/freelancer-rates/freelancer-rates.js
+++ javascript/freelancer-rates/freelancer-rates.js
@@ -1,7 +1,7 @@
 export const dayRate = (ratePerHour) => 8 * ratePerHour;
 
 export const monthRate = (ratePerHour, discount) => 
-  Math.ceil(22 * dayRate(ratePerHour) * (1 - discount));
+  Math.ceil(applyDiscount(22 * dayRate(ratePerHour), discount));
 
 /**
  * Calculates the number of days in a budget, rounded down
@@ -15,13 +15,4 @@ export function daysInBudget(budget, ratePerHour, discount) {
   throw new Error('Implement the daysInBudget function');
 }
 
-/**
- * Applies a discount to the value
- *
- * @param {number} value
- * @param {number} percentage for example 20% written as 0.2
- * @returns {number} the discounted value
- */
-function applyDiscount(value, percentage) {
-  throw new Error('Implement the applyDiscount function');
-}
+const applyDiscount = (value, percentage) =>  value * (1 - percentage);
