e4dedc545e4817d0375a5d5bb322f12af3c26018
Kelly Farlow <kelly.farlow@gmail.com>
2021-10-12 23:52:06 +1000

    javascript/poetry-club-door-policy: Tidy up

--- javascript/poetry-club-door-policy/door-policy.js
+++ javascript/poetry-club-door-policy/door-policy.js
@@ -1,36 +1,9 @@
-/**
- * Respond with the correct character, given the line of the
- * poem, if this were said at the front door.
- *
- * @param {string} line
- * @returns {string}
- */
 export const frontDoorResponse = (line) => line.charAt(0);
-/**
- * Format the password for the front-door, given the response
- * letters.
- *
- * @param {string} word the letters you responded with before
- * @returns {string} the front door password
- */
+
 export const frontDoorPassword = (word) => capitalise(word);
 
-/**
- * Respond with the correct character, given the line of the
- * poem, if this were said at the back door.
- *
- * @param {string} line
- * @returns {string}
- */
 export const backDoorResponse = (line) => line.charAt(line.trim().length - 1);
 
-/**
- * Format the password for the front-door, given the response
- * letters.
- *
- * @param {string} word the letters you responded with before
- * @returns {string} the back door password
- */
 export const backDoorPassword = (word) => `${capitalise(word)}, please`;
 
 const capitalise = (word) => `${word.charAt(0).toUpperCase()}${word.slice(1).toLowerCase()}`
